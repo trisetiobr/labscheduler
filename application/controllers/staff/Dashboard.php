850a303063528782ff1fc1c88abd48110ad49936
trisetiobr <trisetiobr@gmail.com>
2017-10-09 17:00:55 +0700

    set sidebar menu

--- application/controllers/staff/Dashboard.php
+++ application/controllers/staff/Dashboard.php
@@ -12,10 +12,24 @@ class Dashboard extends CI_Controller
 		//check if session exists
 		if($this->session->userdata('id') != '')
 		{
+			$data = array(
+				'total_pengajar'=>0,
+				'total_matakuliah'=>0,
+				'total_laboratorium'=>0,
+				'total_pengguna'=>0,
+				'total_jadwal'=>0,
+				'total_konfirmasi_peminjaman'=>0
+			);
+			//$data['total_pengajar'] =
+			//$data['total_matakuliah'] =
+			//$data['total_laboratorium'] = 
+			//$data['total_pengguna'] =
+			//$data['total_jadwal'] = 
+			//$data['total_konfirmasi_peminjaman'] =
 			$this->load->view('config/header');
 			$this->load->view('staff/main-header');
 			$this->load->view('staff/main-sidebar');
-			$this->load->view('staff/dashboard');
+			$this->load->view('staff/dashboard', $data);
 			$this->load->view('staff/main-footer');
 			$this->load->view('config/footer');
 		}
